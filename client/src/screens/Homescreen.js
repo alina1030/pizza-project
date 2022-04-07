f3202ecfc3d5eeda5a89bd6b425de26c5c04e909
alina1030 <85539852+alina1030@users.noreply.github.com>
2022-04-07 13:27:56 +0530

    Update Homescreen.js

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -21,15 +21,21 @@ export default function Homescreen() {
         <div className='row justify-content-center'>
             <Filter/>
             
-      { pizzas.map((pizza) => {
-                return (
-                <div className='col-md-3 m-3' key={pizza._id}>
-                    <div>
-                        <Pizza pizza={pizza} />
-                    </div>    
-                </div>   
-            );    
-            })}
+      {loading ? (
+          <Loading/>
+        ) : error ? (
+          <Error error='Something went wrong'/>
+        ) : (
+          pizzas.map((pizza) => {
+            return (
+              <div className="col-md-3 m-3" key={pizza._id}>
+                <div>
+                  <Pizza pizza={pizza} />
+                </div>
+              </div>
+            );
+          })
+        )}
             
         </div>
     </div>
